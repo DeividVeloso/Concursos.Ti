8a46e1aa2e627c3c0d86f891faaf94b5cdbe96e1
SoulReaperNZ <veloso.deivid@gmail.com>
2016-11-11 06:31:48 -0200

    Java

---
+++ Veloso.CalculaHorasEstudos/Scripts/calcula.js
@@ -0,0 +1,15 @@
+﻿function postHoursToMinute(evento) {
+    evento.preventDefault();
+    let dadosSerializado = $('#formDados').serealize();
+    $.ajax({
+        url: 'http://localhost:59509/api/MinutesToHours',
+        contentType: "application/json",
+        dataType: 'json',
+        type: 'post',
+        data: dadosSerializado,
+        sucess: function (resposta) { console.log(resposta) },
+        error: function (resposta) { console.log("erro") }
+
+    });
+
+};
